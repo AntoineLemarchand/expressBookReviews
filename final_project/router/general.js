8c78d14ac54a2d4840d390d99a2ce67add1be75c
alemarch <antoine.lemarchand@mailbox.org>
2023-08-03 05:24:24 -0400

    final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,122 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  if (!books[isbn])
+    return res.status(404).json({message: "Book not found"})
+  return res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author
+  let booksByAuthor = []
+  for (isbn in books) {
+      if (books[isbn].author === author) {
+        booksByAuthor.push(books[isbn])
+      }
+  }
+  return res.status(200).json(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title
+    let booksWithTitle = []
+    for (isbn in books) {
+        if (books[isbn].title === title) {
+          booksWithTitle.push(books[isbn])
+        }
+    }
+    return res.status(200).json(booksWithTitle);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn
+    if (!books[isbn])
+      return res.status(404).json({message: "Book not found"})
+    return res.status(200).json(books[isbn].reviews);
 });
+
+async function getBooks() {
+  try {
+    const response = await axios.get('/books');
+    const books = response.data;
+    return books;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to get books');
+  }
+}
+
+// get books with Promise
+async function getBooks() {
+    try {
+      const response = await axios.get('/');
+      const books = response.data;
+      return books;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to get books');
+    }
+  }
+
+// get book from isbn with Promise
+async function getBookFromIsbn(isbn) {
+    try {
+      const response = await axios.get(`/isbn/${isbn}`);
+      const book = response.data;
+      return book;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to get book');
+    }
+  }
+
+// get books from author with Promise
+async function getBooksFromAuthor(author) {
+    try {
+      const response = await axios.get(`/author/${author}`);
+      const books = response.data;
+      return books;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to get book');
+    }
+  }
+
+// get books from title with Promise
+async function getBooksFromTitle(title) {
+    try {
+      const response = await axios.get(`/title/${title}`);
+      const books = response.data;
+      return books;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to get book');
+    }
+  }
+
 
 module.exports.general = public_users;
